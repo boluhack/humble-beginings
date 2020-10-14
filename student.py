d2402e509fd71ab4918de233d0111587f7b993e7
cars45 <cars45@Dealwaps-MacBook-Pro.local>
2020-10-14 16:53:05 +0100

    Create cars45 home page

---
+++ student.py
@@ -0,0 +1,9 @@
+from tkinter import*
+class Student:
+    def __init__(self,root):
+        self.root=root
+        self.root.title("Student Managemenht System")
+        self.root.geometry("1350x700+0+0")
+
+
+root=Tk()
